edb4ab1ba0af3875c136f4efb5204c6d097d9da7
pjanil <anilignatious@ymail.com>
2017-06-02 17:37:38 -0400

    BR Engine C#
    A Business Rule Engine is a software program which executes one or more business rules on behalf of other programs. This software provides convention, grammar or UI Interface which can be used by Business Person. They can define the rules without knowing the programming. Rules can be changed dynamically without changing the application code.

---
+++ caseProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMax.MarketingTech.RulesEngine
+{
+    class caseProcessor
+    {
+        #region applyCaseRules
+         public static bool caseProc(ConcurrentDictionary<string, string> row, int ruleno, int maxCase, RulesXmlReader x, LogicProcessing logicob, List<string[]> totalLists)
+        {
+            int currentCaseno = 1; bool endRule=false;
+
+            while (currentCaseno <= maxCase)
+            {
+                var caseCond = x.RulesCaseConditions().FirstOrDefault(xx => xx.Key == currentCaseno).Value;
+                var caseStat = x.RulesCaseStatements().FirstOrDefault(xxy => xxy.Key == currentCaseno).Value;
+                if (caseCond == null || caseCond.Count == 0)
+                {
+                    break;
+                }
+                if (logicob.ProcessConditions(ruleno, caseCond.FirstOrDefault().ToString(), inputParser.tableAttributess[ruleno].ToString(), row, totalLists))
+                {
+                    foreach (var item in caseStat)
+                    {
+                        var stm=logicob.ProcessStatements(currentCaseno, item, inputParser.tableAttributess[ruleno].ToString(), row, totalLists);
+                       if(stm)
+                       {
+                           return true;
+                       }
+                    }
+                }
+                currentCaseno++;
+            }
+            return endRule;
+
+        }
+       #endregion applyCaseRules
+    }
+}
